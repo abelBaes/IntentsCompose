4865d0c3c7fefc62f04e187154e62a24d6b38f87
abelBaes <abel.baes77@gmail.com>
2026-09-15 23:31:05 -0300

    feat(MainActivity): adicionando mainNavHost

--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3039307/intentscompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3039307/intentscompose/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import br.edu.ifsp.scl.prdm.sc3039307.intentscompose.navigation.MainNavHost
 import br.edu.ifsp.scl.prdm.sc3039307.intentscompose.ui.theme.IntentsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +20,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navHostController = rememberNavController()
             IntentsComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainNavHost(
+                        navHostController = navHostController,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IntentsComposeTheme {
-        Greeting("Android")
     }
 }
